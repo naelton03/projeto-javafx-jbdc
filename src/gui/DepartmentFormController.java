c6ec2e1823b489788df4bac97fc9dc69f31ebe53
naelton <naeltomf@gmail.com>
2020-04-17 20:47:55 -0300

    Passando OBJ Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
-	
+
+	private Department entity;
+
 	@FXML
 	private TextField txtId;
 
@@ -20,32 +23,44 @@ public class DepartmentFormController implements Initializable {
 
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
 	@FXML
 	public void onBtSaveAction() {
 		System.out.println("Click On");
 	}
-	
+
 	@FXML
 	public void onBtCancelAction() {
 		System.out.println("Click On");
 	}
 
-	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
 	}
-	
+
 	public void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+	public void updateFormData() {
+
+		if (entity == null) {
+			throw new IllegalStateException("Entity as null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtId.setText(String.valueOf(entity.getName()));
 	}
 
 }
