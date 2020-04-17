608d1337fd88ff7d4ee258d9951cfd14eabef17e
naelton <naeltomf@gmail.com>
2020-04-16 23:54:17 -0300

    MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,10 +14,10 @@ public class Main extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			
+
 			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
