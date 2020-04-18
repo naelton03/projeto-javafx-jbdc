e35c540bc3e17487a4c7dd005dc9b778c2b6f386
naelton <naeltomf@gmail.com>
2020-04-17 21:10:42 -0300

    Save a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -47,6 +47,7 @@ public class DepartmentListController implements Initializable {
 
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
+		
 		Stage parentStage = Utils.currentStage(event);
 		Department obj =  new  Department();
 		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
@@ -90,6 +91,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
 
 			Stage dialogStage = new Stage();
